85f8eb850445af200a175a8efdc40b52043916b9
zhhtao <375531039@qq.com>
2016-07-01 19:02:40 +0800

    增加图片的本地保存和读取

--- GetPicFromCamerAndGallery.java
+++ GetPicFromCamerAndGallery.java
@@ -118,4 +118,111 @@ public class MainActivity extends Activity {
  
          super.onActivityResult(requestCode, resultCode, data);
      }
+	 
+	 
+	 /**
+     * 将Bitmap转换成字符串保存至SharedPreferences
+     * <p/>
+     * 注意:
+     * 在压缩图片至输出流时,不要选择CompressFormat.JPEG而该是PNG,否则会造成图片有黑色背景.
+     * 详见参考资料二
+     */
+    private void saveBitmapToSharedPreferences(Bitmap bitmap) {
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+        //第一步:将Bitmap压缩至字节数组输出流ByteArrayOutputStream
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        //第二步:利用Base64将字节数组输出流中的数据转换成字符串String
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        String imageString = new String(Base64.encodeToString(byteArray, Base64.DEFAULT));
+        //第三步:将String保持至SharedPreferences
+        LogUtil.w("imageString:"+imageString.length());
+        SharedPreferencesUtil.putString(mContext, CommonConstants.USER_PHOTO, imageString);
+//        SharedPreferences sharedPreferences = getSharedPreferences("testSP", Context.MODE_PRIVATE);
+//        Editor editor = sharedPreferences.edit();
+//        editor.putString("image", imageString);
+//        editor.commit();
+    }
+
+
+    /**
+     * 从SharedPreferences中取出Bitmap,并显示在imageview上
+     */
+    private void getBitmapFromSharedPreferences(ImageView mImageView) {
+//        SharedPreferences sharedPreferences = getSharedPreferences("testSP", Context.MODE_PRIVATE);
+        //第一步:取出字符串形式的Bitmap
+//        String imageString = sharedPreferences.getString("image", "");
+        String imageString = SharedPreferencesUtil.getString(mContext, CommonConstants.USER_PHOTO,"");
+        if (imageString.equals("")) {
+            return;
+        }
+
+        //第二步:利用Base64将字符串转换为ByteArrayInputStream
+        byte[] byteArray = Base64.decode(imageString, Base64.DEFAULT);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArray);
+        //第三步:利用ByteArrayInputStream生成Bitmap
+        Bitmap bitmap = BitmapFactory.decodeStream(byteArrayInputStream);
+        mImageView.setImageBitmap(bitmap);
+    }
+	
+	
+	/**
+     * 从本地读取图片
+     *
+     * @param url
+     * @return
+     */
+    public Bitmap getBitmapFromLocal(String url) {
+
+        try {
+            String fileName = url;
+            File file = new File(LOCAL_PATH, fileName);
+
+            if (file.exists()) {
+                // 图片压缩
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;// 表示压缩比例，2表示宽高都压缩为原来的二分之一， 面积为四分之一
+                options.inPreferredConfig = Bitmap.Config.RGB_565;// 设置bitmap的格式，565可以降低内存占用
+
+                Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(
+                        file), null, options);
+                LogUtil.w("get local iv");
+                return bitmap;
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 向本地存图片
+     *
+     * @param url
+     * @param bitmap
+     */
+    public void putBitmapToLocal(String url, Bitmap bitmap) {
+        try {
+            String fileName = url;
+            File file = new File(LOCAL_PATH, fileName);
+            File parent = file.getParentFile();
+
+            // 创建父文件夹
+            if (!parent.exists()) {
+                parent.mkdirs();
+            }
+
+            FileOutputStream fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
  }
